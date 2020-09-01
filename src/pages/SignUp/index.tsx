8fa599d093b155ac4b4718629f7fe8ded0100f5e
Higor <higorhmscebrac@hotmail.com>
2020-08-31 21:03:14 -0300

    feature: sign up

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,35 +1,58 @@
 import React, { useState, useCallback } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Container,
-  ImageContainer,
-  FormContainer,
-  NotHaveAccountContainer,
-} from './styles';
+
+import { useNavigate } from 'react-router';
+import { Container, ImageContainer, FormContainer } from './styles';
 import api from '../../services/api';
 
+interface ISignUpRequest {
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
-  const [loginInput, setLoginInput] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [emailConfirmationInput, setEmailConfirmationInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
+  const [nameInput, setNameInput] = useState('');
+  const [nicknameInput, setNicknameInput] = useState('');
+  const navigate = useNavigate();
 
   const handleSignUp = useCallback(
     async (event): Promise<void> => {
       event.preventDefault();
-
-      await api.post('/sessions', {
-        login: loginInput,
+      console.log('signup');
+      const response = await api.post('/users', {
+        name: nameInput,
+        nickname: nicknameInput,
+        email: emailInput,
         password: passwordInput,
-      });
+      } as ISignUpRequest);
+
+      if (response) navigate('/signin');
     },
-    [loginInput, passwordInput],
+    [emailInput, passwordInput, nameInput, nicknameInput, navigate],
   );
 
-  const handleChangeLoginInput = useCallback((event) => {
-    setLoginInput(event.target.value);
+  const handleChangeEmailInput = useCallback((event) => {
+    setEmailInput(event.target.value);
   }, []);
 
   const handleChangePasswordInput = useCallback((event) => {
     setPasswordInput(event.target.value);
+  }, []);
+
+  const handleChangeNameInput = useCallback((event) => {
+    setNameInput(event.target.value);
+  }, []);
+
+  const handleChangeNicknameInput = useCallback((event) => {
+    setNicknameInput(event.target.value);
+  }, []);
+
+  const handleChangeEmailConfirmationInput = useCallback((event) => {
+    setEmailConfirmationInput(event.target.value);
   }, []);
 
   return (
@@ -44,22 +67,29 @@ const SignUp: React.FC = () => {
           <div>
             <input
               placeholder="Nome: "
-              name="login"
-              onChange={(event) => handleChangeLoginInput(event)}
+              name="name"
+              onChange={(event) => handleChangeNameInput(event)}
+            />
+          </div>
+          <div>
+            <input
+              placeholder="Apelido: "
+              name="nickname"
+              onChange={(event) => handleChangeNicknameInput(event)}
             />
           </div>
           <div>
             <input
               placeholder="E-mail: "
               name="email"
-              onChange={(event) => handleChangePasswordInput(event)}
+              onChange={(event) => handleChangeEmailInput(event)}
             />
           </div>
           <div>
             <input
               placeholder="Confirme e-mail: "
               name="emailConfirmation"
-              onChange={(event) => handleChangePasswordInput(event)}
+              onChange={(event) => handleChangeEmailConfirmationInput(event)}
             />
           </div>
           <div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: `${process.env.API_URL}` });
+const api = axios.create({ baseURL: `https://smia-api.herokuapp.com` });
 
 export default api;
