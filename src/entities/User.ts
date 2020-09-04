93d1ece777b054655c70ab22a79bd78e5f772ff1
Higor <higorhmscebrac@hotmail.com>
2020-09-03 21:45:10 -0300

    feat: add cards of complaints

---
+++ src/entities/User.ts
@@ -0,0 +1,8 @@
+export default interface IUser {
+  id: string;
+  name: string;
+  nickname: string;
+  email: string;
+  avatar: string;
+  avatar_url: string;
+}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Container } from './styles';
+import api from '../../services/api';
+import Card from '../../components/Card';
+import IComplaint from '../../entities/Complaint';
 
 const Dashboard: React.FC = () => {
+  const [complaints, setComplaints] = useState([] as IComplaint[]);
+
+  useEffect(() => {
+    api.get('/complaints').then((response) => {
+      setComplaints(response.data);
+    });
+  }, [complaints]);
+
   return (
     <Container>
-      <div>Dashboard</div>
+      {complaints?.map((complaint) => (
+        <Card key={complaint.id} complaint={complaint} />
+      ))}
     </Container>
   );
 };
