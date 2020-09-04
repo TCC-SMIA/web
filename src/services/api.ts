b4a6ea2273bd285accc7360a3cdf445a7c4702ff
Higor <higorhmscebrac@hotmail.com>
2020-09-03 21:16:12 -0300

    fix: ensure url works

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const url = process.env.REACT_APP_API_URL;
+const url =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3333'
+    : process.env.REACT_APP_API_URL;
 
 const api = axios.create({ baseURL: url });
 
