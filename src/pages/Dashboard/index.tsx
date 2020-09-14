07f3be320195faaf31c591974b01cf3d3a13f261
gabrielrportugal <gabrielrportugal@outlook.com>
2020-09-14 17:52:36 -0300

    feat: add select to filter by city and state

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, ChangeEvent } from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
 
-import { Container, Feed } from './styles';
+import { Container, Feed, SearchSelect, SearchContainer } from './styles';
 import api from '../../services/api';
 import Card from '../../components/Card';
 import IComplaint from '../../entities/Complaint';
 
+interface IBGECityResponse {
+  nome: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
 const Dashboard: React.FC = () => {
   const [complaints, setComplaints] = useState([] as IComplaint[]);
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
 
   useEffect(() => {
     api.get('/complaints').then((response) => {
@@ -14,8 +27,79 @@ const Dashboard: React.FC = () => {
     });
   }, []);
 
+  useEffect(() => {
+    api
+      .get<IBGEUFResponse[]>(
+        'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === '0') return;
+
+    api
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`,
+      )
+      .then((response) => {
+        const cityNames = response.data.map((city) => city.nome);
+
+        setCities(cityNames);
+      });
+  }, [selectedUf]);
+
+  const handleSelectUf = useCallback(
+    (event: ChangeEvent<HTMLSelectElement>): void => {
+      setSelectedUf(event.target.value);
+    },
+    [],
+  );
+
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement>): void {
+    setSelectedCity(event.target.value);
+  }
+
   return (
     <Container>
+      <SearchContainer>
+        <SearchSelect>
+          <select
+            name="uf"
+            id="uf"
+            onChange={handleSelectUf}
+            value={selectedUf}
+          >
+            <option value="0">Selecione uma UF</option>
+            {ufs.map((uf) => (
+              <option key={uf} value={uf}>
+                {uf}
+              </option>
+            ))}
+          </select>
+          <IoIosArrowDown />
+        </SearchSelect>
+        <SearchSelect>
+          <select
+            name="city"
+            id="city"
+            onChange={handleSelectCity}
+            value={selectedCity}
+          >
+            <option value="0">Selecione uma cidade</option>
+            {cities.map((city) => (
+              <option key={city} value={city}>
+                {city}
+              </option>
+            ))}
+          </select>
+          <IoIosArrowDown />
+        </SearchSelect>
+      </SearchContainer>
       <Feed>
         {complaints?.map((complaint) => (
           <Card key={complaint.id} complaint={complaint} />
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -15,3 +15,37 @@ export const Feed = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const SearchSelect = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  flex: 1;
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    flex: 1;
+    background: var(--color-white);
+    border-radius: 8px;
+    border: 0;
+    padding: 16px 24px;
+    font-size: 1rem;
+    margin: 0 8px;
+  }
+
+  option {
+    font-size: 0.9rem;
+  }
+
+  svg {
+    position: absolute;
+    right: 16px;
+  }
+`;
