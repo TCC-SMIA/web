fb43fe3d98bb77af59b46d88456f860184a2edb1
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2020-11-06 21:54:05 -0300

    style: responsividade do report nos smartphones

--- src/pages/Report/styles.ts
+++ src/pages/Report/styles.ts
@@ -3,22 +3,31 @@ import styled from 'styled-components';
 export const Container = styled.div`
   background: var(--color-white);
   width: 40%;
+  min-width: 40%;
   height: 40%;
   border-radius: 8px;
   padding: 30px;
 
+  @media (max-width: 450px) {
+    width: 100%;
+  }
+
   textarea {
     margin-left: 15px;
     margin-right: 15px;
-    min-width: 485px;
+    min-width: 95%;
     min-height: 200px;
-    max-width: 680px;
+    max-width: 700px;
     max-height: 200px;
     border: 1px solid var(--color-separator);
     background: var(--color-textarea);
     border-radius: 8px;
     padding: 10px;
     font-family: 'Poppins', 'Helvetica', 'Arial', sans-serif;
+
+    @media (max-width: 450px) {
+      width: 95%;
+    }
   }
 
   footer {
@@ -89,12 +98,16 @@ export const OptionMap = styled.div`
 
   margin: 0 20px;
 
-  min-width: 465px;
+  min-width: 150px;
   max-width: 700px;
   height: 320px;
 
   > div {
     display: flex;
     flex: 1;
+  }
+
+  @media (max-width: 450px) {
+    width: 95%;
   }
 `;
