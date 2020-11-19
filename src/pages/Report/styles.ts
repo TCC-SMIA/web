66a02c55874262245be6adff4c0dcb68ceead8f6
gabrielrportugal <gabrielrportugal@outlook.com>
2020-11-19 16:22:40 -0300

    feat: select complaint type in report page

--- src/pages/Report/styles.ts
+++ src/pages/Report/styles.ts
@@ -101,3 +101,41 @@ export const OptionMap = styled.div`
     width: 95%;
   }
 `;
+
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const SearchSelect = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  flex: 1;
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    flex: 1;
+    background: var(--color-white);
+    border-radius: 8px;
+    border: 0;
+    padding: 16px 24px;
+    font-size: 1rem;
+    margin: 4px 8px;
+  }
+
+  option {
+    font-size: 0.9rem;
+  }
+
+  svg {
+    position: absolute;
+    right: 16px;
+  }
+`;
