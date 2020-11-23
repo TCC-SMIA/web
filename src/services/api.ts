a2538ef4462e0f1a4f8aa5b2dbecfb3e352ab63b
Higor <higorhmscebrac@hotmail.com>
2020-11-23 09:49:49 -0300

    feat: sign out when token is expired

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,13 @@ const url = process.env.REACT_APP_API_URL;
 
 const api = axios.create({ baseURL: url });
 
+api.interceptors.response.use((response) => {
+  if (response.data.status.toString() === '401') {
+    localStorage.clear();
+    window.location.reload();
+  }
+
+  return response;
+});
+
 export default api;
