7ca9be633f915d524ff8696f054525f25f3aec02
Higor <higorhmscebrac@hotmail.com>
2020-11-28 17:02:17 -0300

    style: fix profile container

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  margin-top: 10px;
 `;
 
 export const FormContent = styled.div`
